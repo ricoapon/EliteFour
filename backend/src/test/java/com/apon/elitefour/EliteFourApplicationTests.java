c3bf7e6611cb8e0e303a3ee6f3d1b88d1d7b398e
Rico Apon <rico.apon@hotmail.com>
2020-01-19 16:27:00 +0100

    Add basic Spring Boot application

---
+++ backend/src/test/java/com/apon/elitefour/EliteFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apon.elitefour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
