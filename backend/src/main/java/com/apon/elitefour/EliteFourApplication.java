99a54f6d9bc36dac5f76360b25c4cd0b6a5f52e3
Rico Apon <rico.apon@hotmail.com>
2020-01-19 20:58:33 +0100

    Fix weird spacing

--- backend/src/main/java/com/apon/elitefour/EliteFourApplication.java
+++ backend/src/main/java/com/apon/elitefour/EliteFourApplication.java
@@ -1,13 +1,13 @@
 package com.apon.elitefour;
 
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EliteFourApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EliteFourApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EliteFourApplication.class, args);
+    }
 
 }
